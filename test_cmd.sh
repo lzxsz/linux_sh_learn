fac21003ce73c64aa503353fe897e8263cdea5a4
davylin <davylin@163.com>
2019-03-24 17:37:41 +0800

    add test command example

---
+++ test_cmd.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+num1=100
+num2=100
+if test $num1 -eq $num2
+then
+   echo "Two numbers are equal!"
+else
+   echo "Two numbers are not equal!"
+fi
+
+
+# -eq  --equal
+# -ne  --not equal
+# -gt  --greater than
+# -ge  --greater than or equal to
+# -lt  --less than
+# -le  --less than or equal to
