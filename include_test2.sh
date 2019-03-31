31f4ae0560c2a31aa09623c34d29bb0969b70f9c
davylin <davylin@163.com>
2019-04-01 02:18:23 +0800

    add include shell file example

---
+++ include_test2.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#Use (.) to refer to the include_test1.sh file
+. ./include_test1.sh
+
+# Or use the following containing file code
+# source ./include_test1.sh
+
+echo "Runoob Website：$url"
