37ef9fffe0913461d35543487ac168f46a5ab38c
davylin <davylin@163.com>
2019-04-01 01:56:30 +0800

    add shell printf format example

---
+++ printf_format.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+ 
+printf "%-10s %-8s %-4s\n" Name Sex Weightkg  
+printf "%-10s %-8s %-4.2f\n" Davy male 66.1234 
+printf "%-10s %-8s %-4.2f\n" Jack male 48.6543 
+printf "%-10s %-8s %-4.2f\n" Rose female 47.9876 
